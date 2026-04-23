4a1217c73598e48af6be64c9a4e50cd1eb17420c
Sanjay A V <sanjayav@Sanjays-MacBook-Air.local>
2026-04-23 22:17:17 +0530

    feat: initial wisesplit backend setup

---
+++ src/main/java/com/sanjay/splitwise/entity/Group.java
@@ -0,0 +1,24 @@
+package com.sanjay.splitwise.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/sanjay/splitwise/entity/GroupMember.java
@@ -0,0 +1,30 @@
+package com.sanjay.splitwise.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "group_members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    private LocalDateTime joinedAt;
+}
