047b24890d4151815354a1b59b851c6911575580
Sanjay A V <sanjayav@Sanjays-MacBook-Air.local>
2026-05-10 09:40:46 +0530

    feat: added addExpense

---
+++ src/main/java/com/sanjay/splitwise/service/ExpenseService.java
@@ -0,0 +1,79 @@
+package com.sanjay.splitwise.service;
+
+import com.sanjay.splitwise.dto.ExpenseRequestDTO;
+import com.sanjay.splitwise.dto.SplitDTO;
+import com.sanjay.splitwise.entity.Expense;
+import com.sanjay.splitwise.entity.ExpenseSplit;
+import com.sanjay.splitwise.entity.Group;
+import com.sanjay.splitwise.entity.User;
+import com.sanjay.splitwise.repository.ExpenseRepository;
+import com.sanjay.splitwise.repository.ExpenseSplitRepository;
+import com.sanjay.splitwise.repository.GroupRepository;
+import com.sanjay.splitwise.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+    private final ExpenseSplitRepository expenseSplitRepository;
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository, ExpenseSplitRepository expenseSplitRepository, UserRepository userRepository, GroupRepository groupRepository){
+        this.expenseRepository = expenseRepository;
+        this.expenseSplitRepository = expenseSplitRepository;
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
+    }
+
+    @Transactional
+    public Expense addExpense(ExpenseRequestDTO request){
+
+//        Fetch Payer
+        User paidBy = userRepository.findById(request.getPaidByUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+//        Fetch group
+        Group group = groupRepository.findById(request.getGroupId())
+                .orElseThrow(() -> new RuntimeException("Group not found"));
+
+//        Validate splits sum
+        BigDecimal total = BigDecimal.ZERO;
+
+        for(SplitDTO split: request.getSplits()){
+            total = total.add(split.getShareAmount());
+        }
+        if(total.compareTo(request.getAmount()) != 0){
+            throw new RuntimeException("Split amounts do not match total expense");
+        }
+
+//        Create Expense
+        Expense expense = Expense.builder()
+                .amount(request.getAmount())
+                .paidBy(paidBy)
+                .group(group)
+                .description(request.getDescription())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Expense savedExpense = expenseRepository.save(expense);
+
+//        Splits
+        for(SplitDTO splitDTO: request.getSplits()){
+            User user = userRepository.findById(splitDTO.getUserId())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+            ExpenseSplit split = ExpenseSplit.builder()
+                    .expense(savedExpense)
+                    .user(user)
+                    .shareAmount(splitDTO.getShareAmount())
+                    .build();
+
+            expenseSplitRepository.save(split);
+        }
+
+        return savedExpense;
+    }
+}
