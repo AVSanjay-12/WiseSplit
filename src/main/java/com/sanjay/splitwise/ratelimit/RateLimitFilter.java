6c1e50fa05cb9dfc745b37c8c19842a5fac3b23d
Sanjay A V <sanjayav1262@gmail.com>
2026-06-03 23:11:34 +0530

    feat: implemented redis based rate limiting

--- src/main/java/com/sanjay/splitwise/ratelimit/RateLimitFilter.java
+++ src/main/java/com/sanjay/splitwise/ratelimit/RateLimitFilter.java
@@ -1,69 +1,79 @@
 package com.sanjay.splitwise.ratelimit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.bucket4j.*;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class RateLimitFilter extends OncePerRequestFilter {
 
-    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+    private final RedisTemplate<String, Integer> redisTemplate;
 
-    private Bucket createNewBucket() {
+    public RateLimitFilter(RedisTemplate<String, Integer> redisTemplate) {
 
-        Bandwidth limit = Bandwidth.builder()
-                .capacity(5)
-                .refillGreedy(5, Duration.ofMinutes(1))
-                .build();
-
-        return Bucket.builder()
-                .addLimit(limit)
-                .build();
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
         String path = request.getRequestURI();
 
-        boolean shouldLimit = request.getMethod().equals("POST")
-                && (path.equals("/auth/login") || path.equals("/users"));
+        boolean shouldLimit = path.equals("/auth/login") || path.equals("/users");
 
         if (!shouldLimit) {
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(
+                    request,
+                    response
+            );
+
             return;
         }
 
         String ip = request.getRemoteAddr();
-        Bucket bucket = cache.computeIfAbsent(ip, k -> createNewBucket());
 
-        if (bucket.tryConsume(1)) {
-            filterChain.doFilter(request, response);
-        } else {
+        if (isRateLimited(ip)) {
+
             response.setStatus(429);
-            response.setContentType(
-                    "application/json"
-            );
+            response.setContentType("application/json");
 
             new ObjectMapper().writeValue(
                     response.getOutputStream(),
-
                     Map.of(
                             "status", 429,
                             "error", "Too Many Requests",
                             "message",
-                            "Rate limit exceeded. Try again later."
+                            "Rate limit exceeded"
                     )
             );
+
+            return;
         }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isRateLimited(String ip) {
+
+        String key = "rate_limit:" + ip;
+
+        Long count = redisTemplate.opsForValue().increment(key);
+
+        if (count != null && count == 1) {
+
+            redisTemplate.expire(key, Duration.ofMinutes(1));
+        }
+
+        return count != null && count > 5;
     }
 }
